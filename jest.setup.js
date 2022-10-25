d8172cecb00f8a0fed46d50120280895f23b1b8f
rjuarezq <juarezqricardo@gmail.com>
2022-10-25 16:09:02 -0500

    Set to read environment variable from test

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+
+require('dotenv').config( {
+    path: '.env.test'
+});
+
+jest.mock( './src/helpers/getEnvironments', () => ({
+    getEnvironments: () => ({...process.env})
+}));
