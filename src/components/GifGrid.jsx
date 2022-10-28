2e338f256ad51e45a17061b3579c172a5a744e50
rjuarezq <juarezqricardo@gmail.com>
2022-10-28 15:24:26 -0500

    Add propType to ensure that title and url have been received correctly

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,6 @@
 import { useFetchGifs } from "../hooks/useFetchGifs";
+import PropTypes from 'prop-types';
+
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
@@ -26,4 +28,8 @@ export const GifGrid = ({ category }) => {
                 </div>
         </div>
     );
+}
+
+GifGrid.propTypes = {
+    category : PropTypes.string.isRequired
 }
